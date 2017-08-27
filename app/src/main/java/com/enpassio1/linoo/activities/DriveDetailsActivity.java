4ede7d54408c90f917fe7c877aa3a91dfab49a76
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-08-28 01:21:23 +0530

    inflated the detailsActivity with the selected/clicked drive data

--- app/src/main/java/com/enpassio1/linoo/activities/DriveDetailsActivity.java
+++ app/src/main/java/com/enpassio1/linoo/activities/DriveDetailsActivity.java
@@ -2,11 +2,18 @@ package com.enpassio1.linoo.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.enpassio1.linoo.R;
 import com.enpassio1.linoo.models.UpcomingDrives;
 
 public class DriveDetailsActivity extends AppCompatActivity {
+
+    TextView companyNameTextView;
+    TextView hiringDateTextView;
+    TextView hiringLocationTextView;
+    TextView hiringPositionTextView;
+    TextView driveDetailsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +21,19 @@ public class DriveDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_drive_details);
         Bundle driveDetailsBundle = getIntent().getBundleExtra("driveDetailsBundle");
         UpcomingDrives currentUpcomingDrive = driveDetailsBundle.getParcelable("currentUpcomingDrives");
+
+        //instantiate the views
+        companyNameTextView = (TextView) findViewById(R.id.company_name_text_view);
+        hiringDateTextView = (TextView) findViewById(R.id.hiring_date_text_view);
+        hiringLocationTextView = (TextView) findViewById(R.id.hiring_place_text_view);
+        hiringPositionTextView = (TextView) findViewById(R.id.job_title_text_view);
+        driveDetailsTextView = (TextView) findViewById(R.id.job_description_text_view);
+
+        //inflate the views
+        companyNameTextView.setText(currentUpcomingDrive.getCompanyName());
+        hiringDateTextView.setText(currentUpcomingDrive.getDriveDate());
+        hiringLocationTextView.setText(currentUpcomingDrive.getPlace());
+        hiringPositionTextView.setText(currentUpcomingDrive.getJobPosition());
+        driveDetailsTextView.setText(currentUpcomingDrive.getDetailedDescription());
     }
 }
