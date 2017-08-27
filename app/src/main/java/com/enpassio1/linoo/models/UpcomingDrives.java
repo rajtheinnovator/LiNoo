7091690799cd79bc30c6725e5d1151898a52d7c6
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-08-28 00:06:14 +0530

    issue of getting null fixed. Now everything is working fine

--- app/src/main/java/com/enpassio1/linoo/models/UpcomingDrives.java
+++ app/src/main/java/com/enpassio1/linoo/models/UpcomingDrives.java
@@ -1,88 +1,49 @@
 package com.enpassio1.linoo.models;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 /**
  * Created by ABHISHEK RAJ on 8/26/2017.
  */
 
 //parcelable created using: http://www.parcelabler.com/
-public class UpcomingDrives implements Parcelable {
-    private String mDriveDate;
-    private String mPlace;
-    private String mDetailedDescription;
-    private String mJobPosition;
-    private String mCompanyName;
+public class UpcomingDrives {
+    private String driveDate;
+    private String place;
+    private String detailedDescription;
+    private String jobPosition;
+    private String companyName;
+
 
     public UpcomingDrives() {
         //required empty constructor, as Firebase was complaining about this being not present
     }
 
-    public UpcomingDrives(String companyName, String driveDate, String place, String position, String detailedDescription) {
-        mCompanyName = companyName;
-        mDriveDate = driveDate;
-        mPlace = place;
-        mDetailedDescription = detailedDescription;
-        mJobPosition = position;
+    public UpcomingDrives(String name, String date, String location, String jobTitle, String description) {
+        companyName = name;
+        driveDate = date;
+        place = location;
+        detailedDescription = description;
+        jobPosition = jobTitle;
     }
 
     public String getDriveDate() {
-        return mDriveDate;
+        return driveDate;
     }
 
     public String getPlace() {
-        return mPlace;
+        return place;
     }
 
     public String getDetailedDescription() {
-        return mDetailedDescription;
+        return detailedDescription;
     }
 
     public String getJobPosition() {
-        return mJobPosition;
+        return jobPosition;
     }
 
     public String getCompanyName() {
-        return mCompanyName;
+        return companyName;
     }
 
 
-    protected UpcomingDrives(Parcel in) {
-        mDriveDate = in.readString();
-        mPlace = in.readString();
-        mDetailedDescription = in.readString();
-        mJobPosition = in.readString();
-        mCompanyName = in.readString();
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(mDriveDate);
-        dest.writeString(mPlace);
-        dest.writeString(mDetailedDescription);
-        dest.writeString(mJobPosition);
-        dest.writeString(mCompanyName);
-    }    @SuppressWarnings("unused")
-    public static final Parcelable.Creator<UpcomingDrives> CREATOR = new Parcelable.Creator<UpcomingDrives>() {
-        @Override
-        public UpcomingDrives createFromParcel(Parcel in) {
-            return new UpcomingDrives(in);
-        }
-
-        @Override
-        public UpcomingDrives[] newArray(int size) {
-            return new UpcomingDrives[size];
-        }
-    };
 }
