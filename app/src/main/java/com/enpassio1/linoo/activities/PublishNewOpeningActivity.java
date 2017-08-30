5b834f5dd650ba7ab560d59149c84b7b38fb9bdc
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-08-30 21:54:15 +0530

    added date picker for publishNewJobOpening

--- app/src/main/java/com/enpassio1/linoo/activities/PublishNewOpeningActivity.java
+++ app/src/main/java/com/enpassio1/linoo/activities/PublishNewOpeningActivity.java
@@ -1,18 +1,22 @@
 package com.enpassio1.linoo.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.enpassio1.linoo.R;
+import com.enpassio1.linoo.fragments.DatePickerFragment;
 import com.enpassio1.linoo.models.UpcomingDrives;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class PublishNewOpeningActivity extends AppCompatActivity {
+public class PublishNewOpeningActivity extends AppCompatActivity implements DatePickerFragment.OnDateSetListener {
 
     EditText companyNameEditText;
     EditText hiringDateEditText;
@@ -20,6 +24,12 @@ public class PublishNewOpeningActivity extends AppCompatActivity {
     EditText jobPositionEditText;
     EditText jobDescriptionEditText;
     Button publishButton;
+
+    String companyName;
+    String recruitmentDate;
+    String recruitmentPlace;
+    String jobPosition;
+    String driveDetails;
 
     /*
     * Firebase instance variables
@@ -35,8 +45,21 @@ public class PublishNewOpeningActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish_new_opening);
+
         companyNameEditText = (EditText) findViewById(R.id.company_name_edit_text);
         hiringDateEditText = (EditText) findViewById(R.id.recruitment_date_edit_text);
+
+        hiringDateEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    DialogFragment newFragment = new DatePickerFragment();
+                    newFragment.show(getSupportFragmentManager(), "datePicker");
+                } else {
+                    Toast.makeText(getApplicationContext(), "Lost the focus", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
         hiringPlaceEditText = (EditText) findViewById(R.id.recruitment_place_edit_text);
         jobPositionEditText = (EditText) findViewById(R.id.job_position_edit_text);
         jobDescriptionEditText = (EditText) findViewById(R.id.drive_details__edit_text);
@@ -48,18 +71,14 @@ public class PublishNewOpeningActivity extends AppCompatActivity {
         publishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String companyName = companyNameEditText.getText().toString().trim();
-                String recruitmentDate = hiringDateEditText.getText().toString().trim();
-                String recruitmentPlace = hiringPlaceEditText.getText().toString().trim();
-                String jobPosition = jobPositionEditText.getText().toString().trim();
-                String driveDetails = jobDescriptionEditText.getText().toString().trim();
+                companyName = companyNameEditText.getText().toString().trim();
+                recruitmentDate = hiringDateEditText.getText().toString().trim();
+                recruitmentPlace = hiringPlaceEditText.getText().toString().trim();
+                jobPosition = jobPositionEditText.getText().toString().trim();
+                driveDetails = jobDescriptionEditText.getText().toString().trim();
 
                 if (TextUtils.isEmpty(companyName)) {
                     companyNameEditText.setError("Enter the company name");
-                    return;
-                }
-                if (TextUtils.isEmpty(recruitmentDate)) {
-                    hiringDateEditText.setError("Enter date of recruitment");
                     return;
                 }
                 if (TextUtils.isEmpty(recruitmentPlace)) {
@@ -87,5 +106,15 @@ public class PublishNewOpeningActivity extends AppCompatActivity {
                 jobDescriptionEditText.setText("");
             }
         });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+
+        hiringDateEditText.setText(day + "/" + month + "/" + year);
+        hiringDateEditText.setEnabled(false);
+        hiringDateEditText.setFocusable(true);
+        hiringDateEditText.setClickable(true);
+
     }
 }
